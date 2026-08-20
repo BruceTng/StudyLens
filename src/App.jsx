5c63c85230ca13e927b7f4b28a77dcd0bc196c16
Bruce Li <bruceetng@gmail.com>
2026-08-19 23:06:39 -0300

    feat: Adicionando estrutura HTML

--- src/App.jsx
+++ src/App.jsx
@@ -1,65 +1,69 @@
+import sinal from "./assets/sinal.svg"
+import wifi from "./assets/wifi.svg"
+import bateria from "./assets/bateria.svg"
 
+import "./App.css"
 
 const App = () => {
   return (
-    <div>
+    <div id="fora">
+      <section id="celular">
+        <header>
+          <div id="horario">
+            <p>9:30</p>
+          </div> 
 
-      <header>
-        <div id="horario">
-          <p>9:30</p>
-        </div> 
+          <div id="icones">
+            <img src={sinal}/>
+            <img src={wifi}/>
+            <img src={bateria}/>
+          </div>
+        </header>
 
-        <div id="icones">
-          <img></img>
-          <img></img>
-          <img></img>
-        </div>
-      </header>
-
-      <main>
-        <div>
-          <img></img>
-          <p>Galeria</p>
-        </div>
-
-        <section>
+        <main>
           <div>
-          <button id="button-pessoal"></button>
-          <button id="button-estudo"></button>
-        </div>
-
-        <div>
-          <button id="button-nova-imagem"></button>
-          <button id="button-nova-pasta"></button>
-          <button id="button-organizar"></button>
-        </div>
-
-        </section>
-
-        <section id="pessoal">
-          <img></img>
-          <img></img>
-        </section>
-
-        <section id="estudo">
-          <div id="pasta1">
-            <img></img>
-            <button></button>
+            <img/>
+            <p>Galeria</p>
           </div>
 
-          <div id="pasta2">
-            <img></img>
-            <button></button>
+          <section>
+            <div>
+            <button id="button-pessoal"></button>
+            <button id="button-estudo"></button>
           </div>
-        </section>
-      </main>
 
-      <footer>
-        <img></img>
-        <img></img>
-        <img></img>
-      </footer>
+          <div>
+            <button id="button-nova-imagem"></button>
+            <button id="button-nova-pasta"></button>
+            <button id="button-organizar"></button>
+          </div>
 
+          </section>
+
+          <section id="pessoal">
+            <img/>
+            <img/>
+          </section>
+
+          <section id="estudo">
+            <div id="pasta1">
+              <img/>
+              <button></button>
+            </div>
+
+            <div id="pasta2">
+              <img/>
+              <button></button>
+            </div>
+          </section>
+        </main>
+
+        <footer>
+          <img/>
+          <img/>
+          <img/>
+        </footer>
+      </section>
     </div>
   )
 }
